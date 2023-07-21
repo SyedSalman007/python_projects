cd44e744be1947737520da3e8f37c0b4361cd910
SyedSalman007 <97502374+SyedSalman007@users.noreply.github.com>
2023-07-22 02:14:54 +0500

    mail_merge_project
    Send custom mail to multiple users at the same time.

---
+++ mail_merge_project_start/main.py
@@ -0,0 +1,21 @@
+#TODO: Create a letter using starting_letter.txt 
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("./Input/Names/invited_names.txt") as file:
+    names_list = file.readlines()
+
+updated_name_list = [name.strip() for name in names_list]
+
+with open("./Input/Letters/starting_letter.txt") as file:
+    letter_string = file.read()
+
+for name in updated_name_list:
+    final_letter =  letter_string.replace("[name]", name)
+    with open(f"./Output/ReadyToSend/letter_for_{name}.txt", mode="w") as file:
+        file.write(final_letter)
